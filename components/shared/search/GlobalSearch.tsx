757248a4770ce510b08c1f409a7a919721a22e56
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-11-18 21:01:48 +0700

    Sidebar complete & fix light theme

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,19 +1,24 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 
 export const GlobalSearch = () => {
   return (
     <div className="relative w-full max-w-[600px] max-lg:hidden">
-      <div className="background-light-800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
+      <div className="background-light900_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
         <Image
           src="/assets/icons/search.svg"
           alt="search"
           width={24}
           height={24}
-          className="cursor-pointer"
+          className="absolute ml-3 cursor-pointer"
         />
-        <Input />
+        <Input
+          type="text"
+          placeholder="Search globally"
+          defaultValue=""
+          className="paragraph-regular pl-12 no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+        />
       </div>
     </div>
   );
