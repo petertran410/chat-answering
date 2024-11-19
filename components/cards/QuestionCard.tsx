eddbf8a4475520ffd67c2aeb61fb726682c75baf
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-11-19 22:00:35 +0700

    Add local search & no result page

---
+++ components/cards/QuestionCard.tsx
@@ -0,0 +1,51 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+interface QuestionCardProps {
+  _id: string;
+  title: string;
+  tags: {
+    _id: string;
+    name: string;
+  }[];
+  author: {
+    _id: string;
+    name: string;
+    picture: string;
+  };
+  upvotes: string;
+  views: string;
+  answers: Array<object>;
+  createdAt: Date;
+}
+
+const QuestionCard = ({
+  _id,
+  title,
+  tags,
+  author,
+  upvotes,
+  views,
+  answers,
+  createdAt,
+}: QuestionCardProps) => {
+  return (
+    <div className="card-wrapper p-9 rounded-[10px] sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {String(createdAt)}
+          </span>
+          <Link href={`/question/${_id}`}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {title}
+            </h3>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
