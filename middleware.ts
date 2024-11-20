01c697b71dcae09c1cd3598322185262e83aae7d
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-11-20 20:03:06 +0700

    Add form question page

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
-const isPublicRoute = createRouteMatcher(["/(.*)"]);
+// const isPublicRoute = createRouteMatcher(["/(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
