bbff82cde3f957d8178711112032c3b835611336
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-11-23 17:05:00 +0700

    Fix middle ware

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -95,8 +95,8 @@ async function Home() {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
