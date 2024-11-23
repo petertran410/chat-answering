8a53f22624a1dfa515602747c8be5023bd291411
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-11-23 15:18:35 +0700

    Commit

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,6 +1,60 @@
 "use server";
+import Question from "@/database/question.model";
+import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
+import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import User from "@/database/user.model";
+import { revalidatePath } from "next/cache";
 
-export const createQuestion = async (param) => {
+export const getQuestion = async (params: GetQuestionsParams) => {
   try {
-  } catch (error) {}
+    connectToDatabase();
+    const questions = await Question.find({})
+      .populate({ path: "tags", model: Tag, select: "name" }) // Only fetch the "name" field
+      .populate({ path: "author", model: User, select: "username" }) // Only fetch the "username"
+      .sort({ createdAt: -1 });
+
+    return { questions };
+  } catch (error) {
+    console.log("Error ...", error);
+    throw error;
+  }
+};
+
+export const createQuestion = async (param: CreateQuestionParams) => {
+  try {
+    connectToDatabase();
+
+    const { title, content, tags, author, path } = param;
+
+    // Create the question
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    const tagDocuments = [];
+
+    // Create the tags or get them if they already exist
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { upsert: true, new: true }
+      );
+
+      tagDocuments.push(existingTag._id);
+    }
+
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
+
+    // Create an interaction record for the user's ask_question action
+    revalidatePath(path);
+    // Imcrement author's reputation by +5 for creating a question
+  } catch (error) {
+    console.log(error);
+  }
 };
