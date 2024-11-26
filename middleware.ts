2b8c6b5ad92c78ba04366769a156602ddade80aa
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-11-26 19:11:26 +0700

    Add api webhook to public

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/api/webhook",
+  "question/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "community",
+  "jobs",
+]);
 
 // const isPublicRoute = createRouteMatcher(["/(.*)"]);
 
